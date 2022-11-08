0f4ec0045e537fe6d7e944c3fa1b601ca1a34d0c
RavenSpear <727832814@qq.com>
2022-11-08 21:00:12 +0800

    update DeiveList

--- k8s-prom-onos-front/src/components/page/Resource/DeviceList.vue
+++ k8s-prom-onos-front/src/components/page/Resource/DeviceList.vue
@@ -7,7 +7,43 @@
             </el-breadcrumb>
         </div>
         <div class="container">
+            <el-collapse>
+                <el-collapse-item v-for="item in edgeList" :key='item.name'>
+                    <template slot="title">
+                        <p class="title">{{item.name}}</p>
+                    </template>
+                </el-collapse-item>
+            </el-collapse>
         </div>
-        
+
     </div>
 </template>
+
+<script>
+import { getClusterNodes } from "../../../api/index";
+export default {
+    data() {
+        return {
+            edgeData: {},
+            edgeList: []
+        };
+    },
+    created() {
+        this.getEdge();
+    },
+    methods: {
+        async getEdge() {
+            let nodes = await (await getClusterNodes()).data.items;
+            console.log(nodes);
+            for (let i = 0; i < nodes.length; i++) {
+                if (nodes[i].metadata.labels.cluster == "edge") {
+                    let edge = {
+                        name: nodes[i].metadata.name
+                    }
+                    this.edgeList.push(edge)
+                }
+            }
+        }
+    },
+};
+</script>
